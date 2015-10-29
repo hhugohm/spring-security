5d12cf0d1959a27f091bf802abc6e0e7b21b80d5
hector.hidalgo <hector.hidalgo@MX-IT00790.globant.com>
2015-10-29 09:24:14 -0600

    initila import

---
+++ springmvc-security-jdbc/src/main/java/org/neos/mvc/HomeController.java
@@ -0,0 +1,67 @@
+package org.neos.mvc;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/welcome")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Welcome to Web Store!");
+		model.addAttribute("tagline", "The one and only amazing web store");
+		
+		return "welcome";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+		
+		
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalido usuario / password!");
+			//model.addObject("pass", pass);
+			
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+		System.out.println("EN EL CONTROLLER LOG IN");
+
+		return model;
+		
+	}
+	
+	// for 403 access denied page
+		@RequestMapping(value = "/403", method = RequestMethod.GET)
+		public ModelAndView accesssDenied(Principal user) {
+
+			ModelAndView model = new ModelAndView();
+
+			if (user != null) {
+				model.addObject("msg", "Hi " + user.getName() 
+				+ ", you do not have permission to access this page!");
+			} else {
+				model.addObject("msg", 
+				"You do not have permission to access this page!");
+			}
+
+			model.setViewName("403");
+			return model;
+
+		}
+}
---
+++ springmvc-security-jdbc/webapp/WEB-INF/jsp/403.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>HTTP Status 403 - Access is denied</h1>
+	<div><div class="ads-in-post hide_if_width_less_800">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- 728x90 - After2ndH4 -->
+<ins class="adsbygoogle hide_if_width_less_800" 
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-2836379775501347"
+     data-ad-slot="3642936086"
+	 data-ad-region="mkyongregion"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div></div><h2>${msg}</h2>
+</body>
+</html>
